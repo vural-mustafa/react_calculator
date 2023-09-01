b1139aa88ffc23f3a750f35ba35698f25b046897
Mustafa Vural <vuralmustafa474@gmail.com>
2023-09-01 19:39:55 +0300

    react_calculator
    react_calculator

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='calc-wrapper'>
+          <h1> Hesap Makinası </h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
